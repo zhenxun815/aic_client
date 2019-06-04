17c0b3cbe61787d72facf0815ff9adde43a2e971
zhenxun815 <guoyiheng89@gmail.com>
2019-06-04 13:01:29 +0800

    change download info form

--- src/main/java/com/tqhy/client/models/msg/local/DownloadMsg.java
+++ src/main/java/com/tqhy/client/models/msg/local/DownloadMsg.java
@@ -1,10 +1,11 @@
 package com.tqhy.client.models.msg.local;
 
+import com.tqhy.client.models.entity.DownloadInfo;
 import com.tqhy.client.models.enums.DownloadTaskApi;
 import com.tqhy.client.models.msg.BaseMsg;
 import lombok.*;
 
-import java.util.Map;
+import java.io.File;
 
 /**
  * @author Yiheng
@@ -23,8 +24,14 @@ public class DownloadMsg extends BaseMsg {
     private DownloadTaskApi downloadTaskApi;
 
     /**
+     * 下载保存文件夹
+     */
+    @NonNull
+    private File downloadDir;
+
+    /**
      * 下载请求参数
      */
     @NonNull
-    private Map<String, String> requestParamMap;
+    private DownloadInfo downloadInfo;
 }
