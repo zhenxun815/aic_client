09b5f7b65050cec8695b432114863f9b8f1e5ebc
zhenxun815 <guoyiheng89@gmail.com>
2019-06-24 15:01:30 +0800

    merge with master

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -103,12 +103,12 @@ function post_ajax(apiName, ajaxUrl, request, callback) {
 			if (response) {
 				callback(response);
 			} else {
-				alert("问题接口：" + apiName + "\n失败----" + response.desc + response.flag);
+				//alert("问题接口：" + apiName + "\n失败----" + response.desc + response.flag);
 				return false;
 			}
 		},
 		error: function (response) {
-			alert("问题接口：" + apiName + "\n异常错误" + JSON.stringify(response));
+			//alert("问题接口：" + apiName + "\n异常错误" + JSON.stringify(response));
 			return false;
 		}
 	})
