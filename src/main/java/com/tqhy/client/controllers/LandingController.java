77f885d3efccaeac7f2c9fe51878af61b4cb7120
zhenxun815 <guoyiheng89@gmail.com>
2019-07-03 14:09:11 +0800

    extract common methods to base class

--- src/main/java/com/tqhy/client/controllers/LandingController.java
+++ src/main/java/com/tqhy/client/controllers/LandingController.java
@@ -1,37 +1,23 @@
 package com.tqhy.client.controllers;
 
 import com.tqhy.client.config.Constants;
-import com.tqhy.client.models.entity.DownloadInfo;
-import com.tqhy.client.models.entity.SaveDataBody;
-import com.tqhy.client.models.entity.SaveDatas;
-import com.tqhy.client.models.enums.DownloadTaskApi;
-import com.tqhy.client.models.enums.SaveTaskType;
 import com.tqhy.client.models.msg.BaseMsg;
-import com.tqhy.client.models.msg.local.*;
+import com.tqhy.client.models.msg.local.LandingMsg;
+import com.tqhy.client.models.msg.local.VerifyMsg;
 import com.tqhy.client.models.msg.server.ClientMsg;
 import com.tqhy.client.network.Network;
-import com.tqhy.client.service.HeartBeatService;
-import com.tqhy.client.task.DownloadTask;
-import com.tqhy.client.task.SaveFileTask;
-import com.tqhy.client.utils.*;
-import io.reactivex.Observable;
+import com.tqhy.client.utils.FileUtils;
+import com.tqhy.client.utils.GsonUtils;
+import com.tqhy.client.utils.NetworkUtils;
+import com.tqhy.client.utils.PropertyUtils;
 import io.reactivex.schedulers.Schedulers;
-import javafx.application.Platform;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
-import javafx.scene.CacheHint;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
-import javafx.scene.image.Image;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import javafx.stage.Stage;
 import lombok.Getter;
 import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -39,7 +25,6 @@ import org.springframework.web.bind.annotation.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author Yiheng
@@ -49,9 +34,10 @@ import java.util.Optional;
 @RestController
 @Getter
 @Setter
-public class LandingController {
+public class LandingController extends BaseWebviewController {
 
     static Logger logger = LoggerFactory.getLogger(LandingController.class);
+
     @FXML
     private WebView webView;
 
@@ -70,205 +56,17 @@ public class LandingController {
     @Value("${path.data:'/data/'}")
     private String localDataPath;
 
-    @Autowired
-    HeartBeatService heartBeatService;
-
-    @Autowired
-    UploadFileController uploadFileController;
-
-    /**
-     * 判断页面是否需要跳转到登录页,与{@link com.tqhy.client.service.HeartBeatService HeartBeatService} 中
-     * {@code jumpToLandingFlag} 进行双向绑定
-     */
-    private BooleanProperty jumpToLandingFlag = new SimpleBooleanProperty(false);
-
     private WebEngine webEngine;
 
     @FXML
-    private void initialize() {
-        initWebView();
-        initWebAlert();
-        initJumpToLanding();
-        loadPage(Network.SERVER_IP);
+    void initialize() {
+        super.initialize(webView);
+        String initUrl = StringUtils.isEmpty(Network.SERVER_IP) ? connectionUrl : landingUrl;
+        logger.info("init load url is: " + initUrl);
+        loadPage(webView, Network.LOCAL_BASE_URL + initUrl);
         //webEngine.load("https://www.baidu.com");
     }
 
-    private void loadPage(String serverIP) {
-        String initUrl = StringUtils.isEmpty(serverIP) ? connectionUrl : landingUrl;
-        if (!StringUtils.isEmpty(initUrl)) {
-            logger.info("localUrl is: " + initUrl);
-            webEngine.load(Network.LOCAL_BASE_URL + initUrl);
-        }
-    }
-
-
-    /**
-     * 初始化webView设置与webEngine对象
-     */
-    public void initWebView() {
-        logger.info("into init webEngine..");
-        webView.setCache(true);
-        webView.setCacheHint(CacheHint.SPEED);
-        logger.info("init webView complete..");
-        this.webEngine = webView.getEngine();
-    }
-
-    /**
-     * 初始化web页面alert事件监听
-     */
-    private void initWebAlert() {
-        webEngine.setOnAlert(event -> {
-            String data = event.getData() + Constants.MSG_SPLITTER;
-            logger.info("alert data is: " + data);
-
-            String[] split = data.split(Constants.MSG_SPLITTER);
-            switch (split[0]) {
-                case Constants.CMD_MSG_UPLOAD:
-                    //alert('upload;case;' + projectId + ';' + projectName)
-                    //alert('upload;test;' + taskId + ';' + projectName)
-
-                    String uploadType = split[1];
-                    String uploadId = split[2];
-                    String uploadTargetName = split[3];
-                    uploadFileController.openUpload(UploadMsg.with(uploadType, uploadId, uploadTargetName));
-                    break;
-                case Constants.CMD_MSG_DOWNLOAD:
-                    //download;{"fileName":"taskName","imgUrlString":"imgUrl1,imgUrl2"}
-                    int index = data.indexOf(Constants.MSG_SPLITTER);
-                    String jsonStr = data.substring(index + 1);
-
-                    Optional<DownloadInfo> downloadInfoOptional = GsonUtils.parseJsonToObj(jsonStr, DownloadInfo.class);
-                    onDownloadOption(downloadInfoOptional);
-                    break;
-                case Constants.CMD_MSG_SAVE:
-                    /*save;{"fileName":"projectName",
-                        "head":[{"title":"分类名称","key":"name","__id":"gCYIMF"},{"title":"已标注","key":"value","__id":"gcSMlC"},{"title":"占比","key":"per","__id":"37ZTmj"}],
-                        "body":[{"name":"temp","value":2,"per":"8%"},{"name":"牙","value":11,"per":"44%"}]
-                        }*/
-
-                    String dataToSave = split[1];
-
-                    Optional<SaveDatas> saveDataOptional = GsonUtils.parseJsonToObj(dataToSave, SaveDatas.class);
-                    onSaveDataOption(saveDataOptional);
-                    break;
-                case Constants.CMD_MSG_LOGOUT:
-                    heartBeatService.stopBeat();
-                    logout();
-                    break;
-                default:
-                    showAlert(data);
-            }
-        });
-    }
-
-    /**
-     * 执行保存指令
-     */
-    private void onSaveDataOption(Optional<SaveDatas> saveDataOptional) {
-        if (saveDataOptional.isPresent()) {
-            SaveDatas saveDatas = saveDataOptional.get();
-            List<SaveDataBody> body = saveDatas.getBody();
-
-            if (null == body || body.size() == 0) {
-                logger.info("save body is empty...");
-                return;
-            }
-
-            File saveDir = FXMLUtils.chooseDir(null);
-            if (null == saveDir) {
-                return;
-            }
-
-            Observable.fromCallable(SaveFileTask.of(SaveDataMsg.of(SaveTaskType.SAVE_REPORT_TO_CSV, saveDir, saveDatas)))
-                      .subscribeOn(Schedulers.io())
-                      .observeOn(Schedulers.io())
-                      .subscribe(saveDataMsgObservable -> {
-                          saveDataMsgObservable.subscribe(saveDataMsg -> {
-                              Integer saveFlag = saveDataMsg.getFlag();
-                              if (BaseMsg.SUCCESS == saveFlag) {
-                                  logger.info("save success");
-                                  showAlert("保存完毕");
-                              } else {
-                                  logger.info("save fail");
-                                  showAlert("保存失败");
-                              }
-                          });
-                      });
-        } else {
-            logger.info("save optional is null");
-            showAlert("保存失败");
-        }
-    }
-
-    /**
-     * 执行下载指令
-     *
-     * @param downloadInfoOptional
-     */
-    private void onDownloadOption(Optional<DownloadInfo> downloadInfoOptional) {
-
-        if (downloadInfoOptional.isPresent()) {
-            DownloadInfo downloadInfo = downloadInfoOptional.get();
-            String imgUrlString = downloadInfo.getImgUrlString();
-            if (StringUtils.isEmpty(imgUrlString)) {
-                logger.info("download img url is empty");
-                return;
-            }
-
-            File downloadDir = FXMLUtils.chooseDir(null);
-            if (null == downloadDir) {
-                return;
-            }
-
-            Observable.fromCallable(DownloadTask.of(DownloadMsg.of(DownloadTaskApi.DOWNLOAD_PDF, downloadDir, downloadInfo)))
-                      .subscribeOn(Schedulers.io())
-                      .observeOn(Schedulers.io())
-                      .subscribe(downloadMsgObservable ->
-                                         downloadMsgObservable.subscribe(downloadMsg -> {
-                                             Integer downloadFlag = downloadMsg.getFlag();
-                                             if (BaseMsg.SUCCESS == downloadFlag) {
-                                                 logger.info("download success");
-                                                 showAlert("下载完毕");
-                                             } else {
-                                                 logger.info("download fail");
-                                                 showAlert("下载失败");
-                                             }
-                                         }));
-        } else {
-            showAlert("下载失败");
-        }
-    }
-
-    /**
-     * 初始化跳转登录页面逻辑
-     */
-    private void initJumpToLanding() {
-        jumpToLandingFlag.bindBidirectional(heartBeatService.jumpToLandingFlagProperty());
-        jumpToLandingFlag.addListener((observable, oldValue, newValue) -> {
-            logger.info("jumpToLandingFlag changed,oldValue is: " + oldValue + ", newValue is: " + newValue);
-            if (newValue) {
-                Platform.runLater(() -> webEngine.load(Network.LOCAL_BASE_URL + landingUrl));
-                jumpToLandingFlag.set(false);
-                Network.TOKEN = null;
-            }
-        });
-    }
-
-    /**
-     * alert
-     *
-     * @param message
-     */
-    public void showAlert(String message) {
-        Platform.runLater(() -> {
-            Dialog<ButtonType> alert = new Dialog<>();
-            Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
-            stage.getIcons().add(new Image(NetworkUtils.toExternalForm("/static/img/logo_title_light.png")));
-            alert.getDialogPane().setContentText(message);
-            alert.getDialogPane().getButtonTypes().add(ButtonType.OK);
-            alert.showAndWait();
-        });
-    }
 
     @PostMapping("/landing")
     @ResponseBody
@@ -316,7 +114,7 @@ public class LandingController {
 
     @GetMapping("/logout")
     public void logout() {
-        jumpToLandingFlag.set(true);
+        super.logout();
     }
 
     @GetMapping("/user/name")
@@ -358,15 +156,7 @@ public class LandingController {
         return response;
     }
 
-    /**
-     * 向js传值
-     *
-     * @param msg
-     */
-    public void sendMsgToJs(String funcName, String msg) {
-        Object response = webView.getEngine()
-                                 .executeScript(funcName + "('" + msg + "')");
-        String s = (String) response;
-        logger.info("get response: " + s);
+    void sendMsgToJs(String funcName, String msg) {
+        super.sendMsgToJs(webView, funcName, msg);
     }
 }
